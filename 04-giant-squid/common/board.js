cfbf33025512b3d1d16bb1eae46393e1ca624a93
Luke Wood <luke.wood@outlook.com>
2021-12-13 17:23:27 +1100

    added eslint and completed giant squid bingo board object functionality

--- 04-giant-squid/common/board.js
+++ 04-giant-squid/common/board.js
@@ -1,39 +1,64 @@
-const empty = new Array(5).fill(0).map(_ => new Array(5).fill(0))
-function Board(board = empty) {
-  const api = {}
-  const entries = board.flat()
-  const draws = []
-  const lastDrawn = 0
-  let marked = 0
+/**
+ * Creates a board object.
+ * @param {array} board this is a 5 x 5 array of positive numbers including zero
+ * @return {int} the score of the board
+ */
+ module.exports = function board(board) {
+  const matches = [];
+  const flatBoard = board.flat();
+  let win = [];
+  let lastDraw;
 
-  function mark(draw) {
-    draws.push(draw)
-    marked += 1
-    lastDrawn = draw
+  const matchCols = (col) => (match) => (match - col) % 5 === 0;
+  const matchRows = (row) => (match) => {
+    return (match >= row * 5) && (match < row * 5 + 5);
+  };
+  const elementMatchesIndex = (index) => (w) => w === index;
+  const removeElementAtIndex = (indexArray) => {
+    return (_, index) => !indexArray.filter(elementMatchesIndex(index)).length;
+  };
+  const calculateSum = (sum, num) => sum + num;
+
+  /**
+  * Marks a number as drawn if it exists on the board.
+  * @param {int} number the number drawn
+  * @return {int} the score of the board or zero
+  */
+  function draw(number) {
+    lastDraw = number;
+    matches.push(flatBoard.indexOf(number));
+    return hasWon();
   }
 
+  /**
+  * Calculates the boards score.
+  * @return {int} the sum of the unmarked numbers on the
+  * board multiplied by the last number drawn.
+  */
+  function calculateScore() {
+    return flatBoard
+        .filter(removeElementAtIndex(matches))
+        .reduce(calculateSum, 0) * lastDraw;
+  }
+
+  /**
+  * Determines if the board has won given the current draws.
+  * @return {int} the sum of the unmarked numbers on the
+  * board multiplied by the last number drawn.
+  */
   function hasWon() {
-
-  }
-
-  function checkRow() {
-    
-  }
-
-  function score() {
-    return entries.filter(e => !draws.reduce((found, draw, index) => {
-      if (found) break
-      if(e === draw) {
-        draws.splice(index, 1)
+    new Array(5).fill(0).forEach((_, index) => {
+      const winningCol = matches.filter(matchCols(index));
+      const winningRow = matches.filter(matchRows(index));
+      if (winningCol.length === 5 || winningRow.length === 5) {
+        winningCol.length === 5 && (win = winningCol);
+        winningRow.length === 5 && (win = winningRow);
       }
-    }, false)).reduce((sum, entry) => sum + entry, 0) * lastDrawn
+    });
+    return (win.length === 5 || 0) && calculateScore();
   }
 
   return {
-    mark,
-    score,
-    hasWon
-  }
+    draw,
+  };
 }
-
-console.log(Board().mark(1))
